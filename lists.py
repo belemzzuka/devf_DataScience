7bdfacd86bf99d0e10729561e9441f11a6e0f4e8
Belem Garrido <belem.garrido@gmail.com>
2022-07-19 19:13:39 -0500

    listas en Python

---
+++ lists.py
@@ -0,0 +1,26 @@
+list_elements = ["element_1", "element_2", "element_3", "element_4"];
+
+# Imprimir toda la lista
+print(list_elements);
+
+# Access to specific element from the list
+print(list_elements[0]);
+
+# Funcion auxiliar - length list
+print(len(list_elements));
+
+# Declarar tres variables que son una lista vacia
+# list, another, word = [];
+
+# Estructuras de datos
+boolean_list = [ True, False, True ];
+integers_list = [1, 2, 3, 4, 5];
+float_list = [1.5, 2.4, 7.7, 5.9, 3.8]
+
+print(boolean_list);
+print(integers_list);
+print(float_list);
+print(type(boolean_list));
+
+new_list = list((10,20,30,40));
+print(new_list);
