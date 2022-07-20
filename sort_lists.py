d019afe5ace91df2343ae9ed4f4d6d2a67cc9f0c
Belem Garrido <belem.garrido@gmail.com>
2022-07-19 20:40:08 -0500

    more sorting lists in Python

--- sort_lists.py
+++ sort_lists.py
@@ -1,12 +1,13 @@
 def helper(number):
     return abs(number - 10) #Esta funcion que definimos es para ordenarlos conforme estén mas cerca del numero 10
 
-countries = [ "MX", "US", "CAN", "POL" ];
-numbers = [3,55,215,104,-344, -2]
-
+countries = [ "Mexico", "Estados Unidos", "Canada", "Polonia", "canada", "brasil" ];
 countries.sort();
+countries.sort( key = str.islower, reverse = True ); # Ordena primero los que empiezan con minusculas y luego los de mayusculas
+
+numbers = [ 3, 55, 215, 104, -344, -2]
 numbers.sort(reverse = True)
-numbers.sort(key=helper) #Defines una funcion para ordenar los numeros
+numbers.sort( key = helper ) #Defines una funcion para ordenar los numeros
 
 print(countries);
 print(numbers);
