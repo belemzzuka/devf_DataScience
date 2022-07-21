884edd3cf90f8f1a2d236a4499e071e83c057fc3
Belem Garrido <belem.garrido@gmail.com>
2022-07-20 19:52:46 -0500

    join tuple in Python

--- tuples.py
+++ tuples.py
@@ -1,13 +1,24 @@
-mytuple = ("Sofia", "Pipino", "Renato", "Vaquita", "Warita");
+myTuple = ("Sofia", "Pipino", "Renato", "Vaquita", "Warita");
+myTuple2 = ("Juana", "Pedro", "Luis", "Paola");
 
-print(mytuple[0])
-print(type(mytuple));
+print(myTuple[0])
+print(type(myTuple));
 
 #update
 
 #hack to update tuple
-aux_list = list(mytuple)
+aux_list = list(myTuple)
 aux_list.append("Tito");
 aux_list[1] = "Rita";
-mytuple = tuple(aux_list)
-print(mytuple)
+myTuple = tuple(aux_list)
+print(myTuple)
+
+#Joining tuples
+join_tuple = myTuple + myTuple2
+print(myTuple.count("Juana"))
+
+print(join_tuple)
+
+#this counts how many is for a specific element
+print(join_tuple.count("Sofia"))
+print(join_tuple.count("Paola"))
